091590d47c9c34d970bda9e312d862906c524f61
yousef271 <128397039+yousef271@users.noreply.github.com>
2024-04-24 10:50:00 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var iframe = document.getElementById("fullscreenFrame");
+    if (iframe.requestFullscreen) {
+        iframe.requestFullscreen();
+    } else if (iframe.mozRequestFullScreen) { /* Firefox */
+        iframe.mozRequestFullScreen();
+    } else if (iframe.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        iframe.webkitRequestFullscreen();
+    } else if (iframe.msRequestFullscreen) { /* IE/Edge */
+        iframe.msRequestFullscreen();
+    }
+});
+
+while (true) {
+    console.log("catching informating")
+}
